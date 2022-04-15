3e95340934cc8031a78b2f4eac742f1f5d39ed38
Robin Herzig <robin.hrzg@gmail.com>
2022-04-15 14:09:17 -0500

    Write a generic function chainer

---
+++ 05152022.3.js
@@ -0,0 +1,11 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+const makeNegative = num => num < 0 ? num : num * -1
+
+// An integer, may be positive or negative.
+// A negative integer.
+// 20 -> -20
+// -20 -> -20
+// Use a ternary operator to test whether the parameter is negative. If so, return that number. If not, return that number *  -1.
+
+// https://www.codewars.com/kata/54fb853b2c8785dd5e000957/train/javascript
