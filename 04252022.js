889841eb17bb90f763ccb8341b22ceb1fa10a070
Robin Herzig <robin.hrzg@gmail.com>
2022-04-25 22:22:42 -0500

    Convert an integer to binary

---
+++ 04252022.js
@@ -0,0 +1,10 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+let binToDec = bin => parseInt(bin, 2)
+
+binToDec("1001001")
+
+// P: A string containing a binary number.
+// R: Return an integer.
+// E: ("1001001") -> 73
+// P: Convert the binary string to an integer using parseInt(bin,2.)
