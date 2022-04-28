1555cd47b8a4e97ad9ab909a796694d66c48c763
Robin Herzig <robin.hrzg@gmail.com>
2022-04-28 13:54:56 -0500

    Convert integer to negative value

---
+++ 05152022.2.js
@@ -0,0 +1,13 @@
+// Convert a string into a number.
+
+const stringToNumber = str => Number(str)
+const stringToNumber = str => str * 1
+const stringToNumber = str => str++
+const stringToNumber = str => parseInt(str)
+const stringToNumber = str => str << .5
+
+// A string of a number.
+// An integer of a number.
+// '12' -> 12
+
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
