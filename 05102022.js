37b28898d2c53949f18edfa48bc9f39c740b79fc
Robin Herzig <robin.hrzg@gmail.com>
2022-05-24 12:54:06 -0400

    Create backronyms from string of random letters

--- 05102022.js
+++ 05102022.js
@@ -5,13 +5,13 @@ var makeBackronym = function(string){
   let arr = string.toUpperCase().split('');
   arr = arr.map((elem) => elem = dict[elem]);
   return arr.join(' ');
-};
+}
 
 makeBackronym("dgm")
 
 // P: A string of random letters, no spaces.
 // R: A string of words, each one beginning with a letter from the provided string.
 // E: "dgm" -> "disturbing gregarious mustache"
-// P: Take the string of random letters and split('') it so each letter is an element of an array. Then use the forEach method to replace each letter into a word by assigning it the value of dict['letter']. Then join(' ') the array and return it.
+// P: Take the string of random letters and split('') it so each letter is an element of an array. Then use the map() method to replace each letter into a word by assigning it the value of dict['letter']. Then join(' ') the array and return it.
 
 // https://www.codewars.com/kata/55805ab490c73741b7000064/train/javascript
