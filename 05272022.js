01e1431065676de3836d1f3c9633f5f69f91766f
Robin Herzig <robin.hrzg@gmail.com>
2022-05-27 13:39:30 -0400

    Convert elems of array to charCode push to object

---
+++ 05272022.js
@@ -0,0 +1,13 @@
+// 
+
+function numObj(arr){
+  return arr.map(elem => { return { [elem]: String.fromCharCode(elem) }}
+)}
+
+
+// An array.
+// An object containing strings.
+// Use the map() method to iterate through the array.
+// Create a object with the elem key and then CharCode value.
+
+// https://www.codewars.com/kata/57ced2c1c6fdc22123000316
