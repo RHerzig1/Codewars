05e44445ffed1f4a5b18fdd72c7739254f832823
Robin Herzig <robin.hrzg@gmail.com>
2022-05-28 19:44:34 -0400

    Return a greeting in a provided language

---
+++ 05282022.js
@@ -0,0 +1,31 @@
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+function greet(language) {
+	const welcome = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'}
+
+  return welcome[language] || welcome['english']
+}
+
+// A string indicating the lanuage required.
+// The value of the key, or "Welcome"
+// Create the object containing {language: welcome} pairs.
+// Return the value, or "welcome" if the key doesn't exist.
+
+// https://www.codewars.com/kata/577ff15ad648a14b780000e7
