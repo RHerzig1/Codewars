e34f094c26bb9a54b0ea9f4d9d7b39039420138d
Robin Herzig <robin.hrzg@gmail.com>
2022-05-29 12:21:02 -0400

    Convert voice recognized strings into integers

--- 05292022.js
+++ 05292022.js
@@ -32,8 +32,9 @@ function scoreboard(string) {
 // An array containing two integers.
 // "The score is two three"-> [2, 3]
 // Create an array bank of number strings.
-// Use the filter() method to search for matching words. Assign the each word to a variable.
-// Use the parseInt() method to convert the strings to numbers. (include a condition to convert "nil" to 0)
-// Place the numbers into an array and return.
+// Seperate the string in an array of words.
+// Iterate through both the number list and word list to search for matches.
+// If a match is found, add it to a score variable.
+// Return the score array.
 
 // https://www.codewars.com/kata/577ff15ad648a14b780000e7
