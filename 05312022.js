8f5380c1ea4a1a93b7fdd030352da12f317f3143
Robin Herzig <robin.hrzg@gmail.com>
2022-05-31 18:35:17 -0400

    05312022

---
+++ 05312022.js
@@ -0,0 +1,12 @@
+// You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+
+function billboard(name, price = 30){
+
+}
+
+// 2 parameters: a string and an integer.
+// Return an integer.
+// ('Steve', 20) -> 100
+// Return the product of name.length and price.
+
+// https://www.codewars.com/kata/570e8ec4127ad143660001fd
