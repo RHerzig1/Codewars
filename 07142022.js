3c603a627bec1c633c17b77e1a731cfcca3f24e8
RobinHerzig <robin.hrzg@gmail.com>
2022-07-14 12:50:43 -0500

    Count of characters in string

---
+++ 07142022.js
@@ -0,0 +1,17 @@
+// Tally characters in string
+// In a string of characters, count the total number of times each character appears.
+
+function count (string) {  
+  const object = {}
+  Array.from(string).map(character => object[character] ? object[character] += 1 : object[character] = 1)
+  return object
+}
+
+// A string of characters. Does case matter?
+// Return an object containing each character, and the number of times it appears.
+// 'abcab' -> {'a': 2, 'b': 2, 'c': 1}
+// Declare an empty object
+// Iterate through the string to evaluate each character.
+// If the character is not already in the object, then add it.
+// Else, add 1 to the value.
+// Return the object when iteration is complete.
