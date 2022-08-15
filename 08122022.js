99c2052029ce5dad86866497b5bcfb17b33b456e
RobinHerzig <robin.hrzg@gmail.com>
2022-08-14 20:06:48 -0500

    Abbreviate words by replacing letters with letter count

--- 08122022.js
+++ 08122022.js
@@ -1,18 +1,35 @@
-// Create a secret holding factory function
-// Write a function that accepts any value as a secret and returns an object containing a getSecret method (returns the secret) and setSecret method (assigns the secret value).
+// Abbreviate words by replacing letters with letter count
+// In the provided string, replace internal letters of the word(s) with a count of the removed letters.
 
-function createSecretHolder(secret) {
-  return {
-    getSecret: function() {
-      return secret
-    },
-    setSecret: function(value) {
-      secret = value
+function abbreviate(string) {
+  const results = []
+  const letterTest = function(letter) {
+    return letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122 || letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90 ? true : false
+  }
+  let letterCount = 0
+  string = [...string]
+  for (let i = 0; i < string.length; i++) {
+    if (letterTest(string[i]) === true && letterCount === 0) {
+      letterCount++
+      results.push(string[i])
+    }
+    else if (letterTest(string[i + 1]) === false && letterCount === 0) {
+      results.push(letterCount)
+      letterCount = 0
+    }
+    else if (letterTest(string[i]) === true) {
+      letterCount++
     }
   }
+  return results
 }
 
-// The parameter is an integer.
-// Return the two methods.
-// To write a factory function, write return {} as the statment body.
-// Inside the curly braces, write the two methods as directed.
+// The parameter is a string containing at least one word.
+// Return a string, but convert words longer than 3 letters into "first letter" "count of interior letters" "final letter" format. Preserve capitalization and punctuation.
+// "JavaScript is really fun!" -> "J8t is r4y fun!"
+// Declare a result variable with a value of [].
+// Declare a letterCount variable with a value of 0.
+// Split the string into an array and iterate through it.
+// If it's a letter and letterCount === 0, then letterCount++ and push it to the results array.
+// Else if it's a letter and letterCounnt > 0, then letterCount++.
+// If the next letter is 
