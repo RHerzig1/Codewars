13dc0151e7bcf0efc8bc956b0df758978f9a41b9
RobinHerzig <robin.hrzg@gmail.com>
2022-08-19 12:44:23 -0500

    Optimize an object class

---
+++ 08192022.js
@@ -0,0 +1,8 @@
+// Optimize an object class
+// Transform the provided class and extended class to meet the shorter character count.
+
+class Labrador extends Dog {
+  constructor(name, age, gender, master) {
+    super (name, age, gender, "Labrador", "Large", master, true)
+  }
+}
