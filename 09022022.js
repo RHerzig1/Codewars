ad0359273688388aa898457b5c2bbc812da640f6
RobinHerzig <robin.hrzg@gmail.com>
2022-09-02 17:42:56 -0500

    Concatenate strings without the + operator or concat() and join() methods.

---
+++ 09022022.js
@@ -0,0 +1,11 @@
+// Concatenate strings without the + operator or concat() and join() methods. 
+
+function joinStrings(string1, string2){
+  return `${string1} ${string2}`
+}
+
+// There are two parameters, containing either a string or a number.
+// Return the concatenation of string1 and string2, seperated by a space.
+// ('hello', 'world') -> 'hello world'
+// Concatenate the strings using template literal.
+// Return the result.
