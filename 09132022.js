4ce769ff44841c28ec56d98f8c546faf0723f6eb
RobinHerzig <robin.hrzg@gmail.com>
2022-09-13 08:57:26 -0500

    Is sum of squares from array larger than sum of cubes from another

---
+++ 09132022.js
@@ -0,0 +1,13 @@
+// Is sum of squares from array larger than sum of cubes of another
+
+function arrayMadness(a, b) {
+  let squared = a.reduce((a, b) => a + b ** 2, 0)
+  let cubed = b.reduce((a, b) => a + b ** 3, 0)
+  return squared > cubed
+}
+
+// The parameters are both arrays of numbers with a length of at least 1 element.
+// Return true or false.
+// Declare squared and cubed variables.
+// Set the values to the arrays squared or cubed using the reduce method.
+// Return a boolean stating squared is larger than cubed.
