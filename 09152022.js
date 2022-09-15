49af610ef1785ac4f2f865ff33aa43b24622e73a
RobinHerzig <robin.hrzg@gmail.com>
2022-09-15 15:20:51 -0500

    Compare all elements of array against testing function

---
+++ 09152022.js
@@ -0,0 +1,10 @@
+// Compare all elements of array against testing function
+
+function all(arr, fun) {
+  return arr.every(fun)
+}
+
+// The parameters are an array of integers and a function.
+// Return true or false, whether every element passes the implimented testing function.
+// Use the every() method on arr. Pass fun as the callback function.
+// Return the result.
