1c85253ca63996dfd8c8567a7b6cb9a1f7e1259c
RobinHerzig <robin.hrzg@gmail.com>
2022-09-16 13:32:55 -0500

    Sum of all elements in array

---
+++ 09162022.js
@@ -0,0 +1,10 @@
+// Sum of all elements in array
+
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0)
+};
+
+// The parameters are an array of integers.
+// Return the sum of all the elements or, if the array is empty, 0.
+// Use the reduce method to add the elements.
+// Return the result.
