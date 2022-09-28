3d4697300fba0140e7df1ddc26b7c814b294b83c
RobinHerzig <robin.hrzg@gmail.com>
2022-09-28 18:14:03 -0500

    Return count of array elements that don't contain 5

--- 09282022.js
+++ 09282022.js
@@ -1,12 +1,19 @@
-// Implement line numbering in array
+// Return count of array elements that don't contain 5
 
-const number = function(array) {
-  return array.map((elem, index) => `${index + 1}: ${elem}`)
+function dontGiveMeFive(start, end) {
+  const array = []
+  for (let i = start; i <= end; i++) {
+    if (!String(i).includes('5')) {
+      array.push(i)
+    }
+  }
+  return array.length
 }
 
-// The parameter is an array containing strings.
-// Return the array with each element prefaced by a number. If the array was empty, return an empty array.
-// ["a", "b", "c"] -> ["1: a", "2: b", "3: c"]
-// Map through the array.
-// For each iteration, concatenate index + 1 to the element.
-// Return the modified array.
+// The parameters are an inclusive starting number and an inclusive ending number.
+// Fill the array using the parameters except for numbers including 5. Return the length of the array.
+// 1, 9 -> 8
+// Delcare an empty array.
+// Iterate through the array with a for loop.
+// For each iteration, push the i into the array as long as it doesn't contain 5.
+// Return the length of the array.
