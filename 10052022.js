4dccfeb97367dd56b82b1b797ccd1bd075a35bc1
RobinHerzig <robin.hrzg@gmail.com>
2022-10-04 13:09:47 -0500

    Reverse a string

--- 10052022.js
+++ 10052022.js
@@ -1,4 +1,4 @@
- // Reverse a string
+// Reverse a string
 
 function reverseString(message) {
   return [...message].reverse().join('')
@@ -6,9 +6,9 @@ function reverseString(message) {
 
 reverseString("   The white space at the beginning of this string doesn't match the whitespace at the end. ")
 
- // The parameter is a non-empty string containing letters, spaces, and numbers.
- // Return the string after reversing all the characters.
- // "Hello World!" -> "!dlroW olleH"
- // Split the message.
- // Reverse the message array.
- // Rejoin the message and return it.
+// The parameter is a non-empty string containing letters, spaces, and numbers.
+// Return the string after reversing all the characters.
+// "Hello World!" -> "!dlroW olleH"
+// Split the message.
+// Reverse the message array.
+// Rejoin the message and return it.
