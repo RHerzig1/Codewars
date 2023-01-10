1eb5a57bb6bc519988c2b1bdf5ed8b1912a56fff
Robin H <robin.hrzg@gmail.com>
2023-01-10 10:39:50 -0500

    Use Hofstadter Female and Male sequences to demonstrate mutual recursion

---
+++ 01102023.js
@@ -0,0 +1,15 @@
+// Use Hofstadter Female and Male sequences to demonstrate mutual recursion.
+
+function F(n) {
+	if (n === 0) {
+  	return 1;
+  }
+  return n - M(F(n - 1));
+}
+
+function M(n) {
+	if (n === 0) {
+  	return 0;
+  }
+  return n - F(M(n - 1));
+}
