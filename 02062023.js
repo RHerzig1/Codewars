1c35b6da4ede6accdbd61059907ec63f363d4168
Robin H <robin.hrzg@gmail.com>
2023-02-06 03:32:21 -0600

    Write an algorithm that calculates the number of levels a pyramid could be built given a budget and price per brick

--- 02062023.js
+++ 02062023.js
@@ -17,3 +17,6 @@ console.log(returnNumberOfLevels(9, 2), 1)
 console.log(returnNumberOfLevels(21, 1.5), 3)
 console.log(returnNumberOfLevels(0, 4), 0)
 console.log(returnNumberOfLevels(-1, 4), 0)
+
+// The budget. May be positive or negative. Always a whole number.
+// The price per brick. Always greater than 0. May be a decimal.
