abcd53fcad388d15a747747d63d056293ef94f77
RobinHerzig <robin.hrzg@gmail.com>
2023-02-07 00:07:54 -0600

    Write an algorithm that calculates the number of levels a pyramid could be built given a budget and price per brick

---
+++ 02062023.js
@@ -0,0 +1,19 @@
+// Write an algorithm that calculates then number of levels a pyramind could be given built given a budget and price per brick.
+
+function returnNumberOfLevels(budget, price) {
+  if (price > budget) return 0
+  
+  let level = 1
+  let cost = price
+  
+  while (budget >= ((level + 1) * (level + 1) * price) + cost) {
+    level++
+    cost += (level * level) * price
+  }
+  return level
+}
+
+console.log(returnNumberOfLevels(9, 2), 1)
+console.log(returnNumberOfLevels(21, 1.5), 3)
+console.log(returnNumberOfLevels(0, 4), 0)
+console.log(returnNumberOfLevels(-1, 4), 0)
