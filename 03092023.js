64997b6e2f681cb78521473f91f695eefc30cc9b
RobinHerzig <robin.hrzg@gmail.com>
2023-03-09 22:50:56 -0600

    Write an algorithm that deciphers the secret string

--- 03092023.js
+++ 03092023.js
@@ -28,6 +28,7 @@ function decipherString(str) {
 
 console.log(decipherString("72olle 103doo 100ya"), "Hello good day");
 console.log(decipherString("82yade 115te 103o"), "Ready set go");
+console.log(decipherString(""), "");
 
 // A string containing deciphered words seperated by a space. All letters are lowercase.
 // Correct and return the string. For each string, convert the character code to it's character value and swap the 2nd and last characters.
