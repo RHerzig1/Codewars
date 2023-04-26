b4217dc98ecb86884cada3131bf7fb37d4cce0b7
Robin H <robin.hrzg@gmail.com>
2023-04-26 09:36:19 -0500

    Write a function that returns the binary value of the sum of two numbers

---
+++ 04262023.js
@@ -0,0 +1,15 @@
+// Write a function that returns the binary value of the sum of two numbers.
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+
+console.log(addBinary(1, 1), 10)
+console.log(addBinary(5, 9), 1110)
+console.log(addBinary(51, 12), 111111)
+
+// Two positive integers.
+// Return the binary value of the sum as a number.
+
+// Add the parameters.
+// Use toString(2) to convert to binary.
