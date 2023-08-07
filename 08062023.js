f1449e4280bb053a4a0b01e387098c2f3a07692f
Robin H <robin.hrzg@gmail.com>
2023-08-07 00:15:11 -0500

    Write a function that creates the Fibonacci sequence using memoization

--- 08062023.js
+++ 08062023.js
@@ -1,7 +1,31 @@
 // Write a function that creates the Fibonacci sequence using memoization.
 
-function caculateLCM(arr) {
+const cache = {
+  0: 0,
+  1: 1,
+};
 
+function calcFibonacci(num) {
+  if (Object.hasOwn(cache, num)) {
+    return cache[num];
+  }
+
+  const val = calcFibonacci(num - 1) + calcFibonacci(num - 2);
+  cache[num] = val;
+  return val;
 }
 
+console.log(calcFibonacci(3), 2);
+console.log(calcFibonacci(10), 55);
+console.log(calcFibonacci(12), 144);
 
+// A positive integer > 0. Always a valid number.
+// Return the Fibonacci sequence value at that index.
+
+// Create a cache.
+// Recursive case:
+// If the cache has num, return num.
+// Base case:
+// Calculate num - 1 + num - 2.
+// Add it to the cache.
+// Return it.
