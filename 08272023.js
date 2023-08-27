0d390b65ed951b3cd9531b2100389ebb140f9b3e
Robin H <robin.hrzg@gmail.com>
2023-08-27 10:43:29 -0500

    Write a prototype method that extracts nested object values

---
+++ 08272023.js
@@ -0,0 +1,26 @@
+// Write a prototype method that extracts nested object values
+
+Object.prototype.hash = function (str) {
+  const arr = str.split(".");
+  return arr.reduce((pv, cv) => (pv ? pv[cv] : pv), this);
+};
+
+const obj = {
+  person: {
+    name: "joe",
+    history: {
+      hometown: "bratislava",
+      bio: {
+        funFact: "I like fishing.",
+      },
+    },
+  },
+};
+
+console.log(obj.hash("person.name"), "joe");
+console.log(obj.hash("person.history.bio"), { funFact: "I like fishing." });
+console.log(obj.hash("person.history.homeStreet"), undefined);
+console.log(obj.hash("person.animal.pet.needNoseAntEater"), undefined);
+
+// An string representing the nested directory into an object.
+// Return the value of that directory, or undefined if the directory is invalid.
