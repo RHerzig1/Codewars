8a672664146bf2ac951a12e044fdb6855a1dd974
Robin H <robin.hrzg@gmail.com>
2023-09-10 14:14:52 -0500

    Write a script that queries the customers who visited but did not make a purchase

--- 09102023.sql
+++ 09102023.sql
@@ -1,50 +1,58 @@
--- Write a script that queries from multiple sales tables using join.
+-- Write a script that queries the customers who visited but did not make a purchase.
 
-SELECT
-  product_name,
-  YEAR,
-  price
-FROM
-  sales
-  LEFT JOIN product ON sales.product_id = product.product_id;
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans 
+FROM visits v 
+LEFT JOIN transactions t 
+ON v.visit_id = t.visit_id  
+WHERE t.transaction_id IS NULL 
+GROUP BY v.customer_id;
 
 /*
- Input: 
- Sales table:
- +---------+------------+------+----------+-------+
- | sale_id | product_id | year | quantity | price |
- +---------+------------+------+----------+-------+ 
- | 1       | 100        | 2008 | 10       | 5000  |
- | 2       | 100        | 2009 | 12       | 5000  |
- | 7       | 200        | 2011 | 15       | 9000  |
- +---------+------------+------+----------+-------+
- 
- Product table:
- +------------+--------------+
- | product_id | product_name |
- +------------+--------------+
- | 100        | Nokia        |
- | 200        | Apple        |
- | 300        | Samsung      |
- +------------+--------------+
- 
- Output: 
- +--------------+-------+-------+
- | product_name | year  | price |
- +--------------+-------+-------+
- | Nokia        | 2008  | 5000  |
- | Nokia        | 2009  | 5000  |
- | Apple        | 2011  | 9000  |
- +--------------+-------+-------+
- 
- Explanation: 
- From sale_id = 1, we can conclude that Nokia was sold for 5000 in the year 2008.
- From sale_id = 2, we can conclude that Nokia was sold for 5000 in the year 2009.
- From sale_id = 7, we can conclude that Apple was sold for 9000 in the year 2011.
+Input: 
+Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+
+Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+
+Output: 
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+| 54          | 2              |
+| 30          | 1              |
+| 96          | 1              |
++-------------+----------------+
+
+Explanation: 
+Customer with id = 23 visited the mall once and made one transaction during the visit with id = 12.
+Customer with id = 9 visited the mall once and made one transaction during the visit with id = 13.
+Customer with id = 30 visited the mall once and did not make any transactions.
+Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
+Customer with id = 96 visited the mall once and did not make any transactions.
+As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
  */
 /*
- Two tables. One containing sales information and the other product information.
- Return product_name, year, and price for each sale_id in the Sales table.
- 
+ Two tables. One containing a customer ID and a unique visit ID. Customers with multiple visits will have their customer ID appear multiple times. And a transaction table, if a transaction exists then it'll appear here with the visit_id
+ Return the costumer IDs and the amount of visits they made that does not appear in the transaction table.
+
  Use the join keyword to combine the two tables.
  */
