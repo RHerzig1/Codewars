820cc842187a156e8f5a333b7e8e1eefc9b2fa86
Robin H <robin.hrzg@gmail.com>
2023-09-26 14:06:46 -0500

    Write a query that returns the a sum of online activities made in the last 30 days for each day

---
+++ 09262023.sql
@@ -0,0 +1,44 @@
+-- Write a query that returns the a sum of online activities made in the last 30 days for each day.
+
+SELECT
+  activity_date AS day,
+  COUNT(DISTINCT user_id) AS active_users
+FROM
+  activity
+WHERE
+  activity_date BETWEEN SUBDATE('2019-07-27', 29) AND '2019-07-27'
+GROUP BY
+  activity_date;
+
+/*
+ Input: 
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+
+Output: 
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+ 
+
+Explanation: Note that we do not care about days with zero active users.
+ */
+/*
+ 
+ */
