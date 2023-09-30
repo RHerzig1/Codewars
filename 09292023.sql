a0528ec2ec1dad7b0dbd55a800c9d8ffcbde0856
Robin H <robin.hrzg@gmail.com>
2023-09-29 20:08:09 -0500

    Write a query that returns a list of users and the number of followers each one has

---
+++ 09292023.sql
@@ -0,0 +1,41 @@
+-- Write a query that returns a list of users and the number of followers each one has.
+
+SELECT
+  user_id,
+  COUNT(follower_id) AS followers_count
+FROM
+  followers
+GROUP BY
+  user_id
+ORDER BY
+  user_id;
+
+/*
+ Input: 
+ Followers table:
+ +---------+-------------+
+ | user_id | follower_id |
+ +---------+-------------+
+ | 0       | 1           |
+ | 1       | 0           |
+ | 2       | 0           |
+ | 2       | 1           |
+ +---------+-------------+
+ 
+ Output: 
+ +---------+----------------+
+ | user_id | followers_count|
+ +---------+----------------+
+ | 0       | 1              |
+ | 1       | 1              |
+ | 2       | 2              |
+ +---------+----------------+
+ 
+ Explanation: 
+ The followers of 0 are {1}
+ The followers of 1 are {0}
+ The followers of 2 are {0,1}
+ */
+/*
+ 
+ */
