8642f8c2b70836abe229f4feeee4707ead3d82f1
Robin H <robin.hrzg@gmail.com>
2023-10-19 15:38:28 -0500

    Write a query that returns the second highest salary

--- 10192023.sql
+++ 10192023.sql
@@ -1,10 +1,54 @@
--- Write a query that returns a list 
+-- Write a query that returns the second highest salary.
 
+SELECT
+  MAX(Salary) AS SecondHighestSalary
+FROM
+  Employee
+WHERE
+  Salary < (
+    SELECT
+      MAX(Salary)
+    FROM
+      Employee
+  )
+  
+  /*
+   Example 1:
+   
+   Input: 
+   Employee table:
+   +----+--------+
+   | id | salary |
+   +----+--------+
+   | 1  | 100    |
+   | 2  | 200    |
+   | 3  | 300    |
+   +----+--------+
 
+   Output: 
+   +---------------------+
+   | SecondHighestSalary |
+   +---------------------+
+   | 200                 |
+   +---------------------+
 
-/*
+   Example 2:
+   
+   Input: 
+   Employee table:
+   +----+--------+
+   | id | salary |
+   +----+--------+
+   | 1  | 100    |
+   +----+--------+
 
- */
-/*
- 
- */
+   Output: 
+   +---------------------+
+   | SecondHighestSalary |
+   +---------------------+
+   | null                |
+   +---------------------+
+   */
+  /*
+   
+   */
