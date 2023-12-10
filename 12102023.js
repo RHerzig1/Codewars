5708aa0a862c7af35aef30c62056cc9fac206e31
Robin H <robin.hrzg@gmail.com>
2023-12-10 11:54:12 -0600

    Write a funciton that counts the number of consecutive pairs in an array

--- 12102023.js
+++ 12102023.js
@@ -1,3 +1,5 @@
+// Write a funciton that counts the number of consecutive pairs in an array.
+
 function pairs(arr) {
   let count = 0;
 
@@ -9,8 +11,6 @@ function pairs(arr) {
 
   return count;
 }
-
-// Write a funciton that counts the number of consecutive pairs in an array.
 
 console.log(pairs([1, 2, 5, 8, -4, -3, 7, 6, 5]), 3);
 console.log(pairs([21, 20, 22, 40, 39, -56, 30, -55, 95, 94]), 2);
