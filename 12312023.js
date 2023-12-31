0181dfe97696ea77cdabde38d25de4aabdc80092
Robin H <robin.hrzg@gmail.com>
2023-12-31 14:35:34 -0600

    Write a function that returns whether a number is 'strong'

--- 12312023.js
+++ 12312023.js
@@ -1,17 +1,17 @@
 // Write a function that returns whether a number is "strong".
 
 function isStrong(num) {
-  const factorials = [0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+  const factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
   const sum = [...String(num)].reduce((a, c) => a + factorials[c], 0);
   return num === sum;
 }
 
-// console.log(isStrong(145), true); // (1) + (1 * 2 * 3 * 4) + (1 * 2 * 3 * 4 * 5) = 1 + 24 + 120 = 145
-// console.log(isStrong(1), true);
-// console.log(isStrong(3), false);
-// console.log(isStrong(150), false);
-// console.log(isStrong(2000), false);
-// console.log(isStrong(0), false);
+console.log(isStrong(145), true); // (1) + (1 * 2 * 3 * 4) + (1 * 2 * 3 * 4 * 5) = 1 + 24 + 120 = 145
+console.log(isStrong(1), true);
+console.log(isStrong(3), false);
+console.log(isStrong(150), false);
+console.log(isStrong(2000), false);
+console.log(isStrong(0), false);
 console.log(isStrong(40585), true);
 
 // A positive integer. Always a valid number. 0 <= number <= 2000.
