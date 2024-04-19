5756a4edee0616190a0630fd42f42a1beba75b00
Robin H <robin.hrzg@gmail.com>
2024-04-18 23:58:46 -0500

    Write a function that returns the number of substrings that contain all three characters at least once

---
+++ 04182024.ts
@@ -0,0 +1,39 @@
+// Write a function that returns the number of substrings that contain all three characters at least once.
+
+function numberOfSubstrings(s: string): number {
+  const charMap = new Map<string, number>();
+  let count = 0;
+  let left = 0;
+  let right = 0;
+
+  while (right < s.length) {
+    // Cache the right pointer value.
+    charMap.set(s[right], (charMap.get(s[right]) || 0) + 1);
+
+    // If needed, increment the left pointer.
+    if (right === s.length - 1) {
+      charMap.set(s[left], (charMap.get(s[left]) || 1) - 1);
+      left++;
+    }
+
+    // Verify the condition.
+    if (charMap.get("a")! > 0 && charMap.get("b")! > 0 && charMap.get("c")! > 0) {
+      count++;
+    }
+
+    // Increment the right pointer.
+    right++;
+  }
+
+  return count;
+}
+
+console.log(numberOfSubstrings("abcabc"), 10);
+console.log(numberOfSubstrings("aaacb"), 3);
+console.log(numberOfSubstrings("abc"), 1);
+
+// s: string. Consists only of "a", "b", and "c". 3 <= s.length <= 5*10^4.
+// Return the number of substrings that contain all three characters at least once.
+
+// Use the sliding window method.
+// Keep a count of all the characters using a map.
