014b9c96d874b81958a2ef1d379482668f1b3523
RobinHerzig <robin.hrzg@gmail.com>
2024-08-26 18:31:11 -0500

    Find the maximum achievable number.

---
+++ 08262024.ts
@@ -0,0 +1,8 @@
+// Find the maximum achievable number.
+
+function theMaximumAchievableX(num: number, t: number): number {
+    return num + (t * 2);
+};
+
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+// Seriously, why is this even a leetcode question???
