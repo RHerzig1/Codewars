98bcba9e555af58ded99fc6fbf8d2cc3912c61f3
RobinHerzig <robin.hrzg@gmail.com>
2024-10-08 21:14:19 -0500

    Count distinct subjects taught by each teacher.

---
+++ 10072024.sql
@@ -0,0 +1,11 @@
+-- Count distinct subjects taught by each teacher.
+
+SELECT
+  teacher_id,
+  COUNT(DISTINCT subject_id) AS cnt
+FROM
+  teacher
+GROUP BY
+  teacher_id;
+
+-- https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/
