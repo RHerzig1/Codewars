fc3ff28b8495bfd40bb79edc71332a86d821b00f
RobinHerzig <robin.hrzg@gmail.com>
2024-11-03 20:40:05 -0600

    Calculate the sum of binary tree nodes between two values (optimized).

---
+++ 11032024.ts
@@ -0,0 +1,55 @@
+// Calculate the sum of binary tree nodes between two values (optimized).
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
+  let sum = 0;
+
+  // Recursive solution.
+  (function DFS(node: TreeNode | null): null {
+    if (!node) {
+      return null;
+    }
+
+    const val = node.val;
+    const left = node.left;
+    const right = node.right;
+
+    // Assess the value and increment sum.
+    if (low <= node.val && node.val <= high) {
+      sum += node.val;
+    }
+
+    // Move only to the left and right nodes if the current node is within the respective bound.
+    if (val > low) {
+      DFS(left);
+    }
+
+    if (val < high) {
+      DFS(right);
+    }
+  })(root);
+
+  return sum;
+}
+
+console.log(rangeSumBST([10, 5, 15, 3, 7, null, 18], 7, 15), 32);
+console.log(rangeSumBST([10, 5, 15, 3, 7, 13, 18, 1, null, 6], 6, 10), 23);
+// https://leetcode.com/problems/range-sum-of-bst/
+
+// root: TreeNode | null. 1 <= number of nodes <= 2*10^4. 1 <= Node.val <= 10^5.
+// low: number. 1 <= low <= high.
+// high: number. low <= high <= 10^5.
+// Return the sum of node values where: low >= nod.val <= high.
