ee39543232b202b03ff58e4f1ab18604d6c62a9c
RobinHerzig <robin.hrzg@gmail.com>
2025-01-23 23:51:55 -0600

    Select the name of employees that meet salary and longevity criteria.

---
+++ 01232025.sql
@@ -0,0 +1,5 @@
+-- Select the name of employees that meet salary and longevity criteria.
+
+SELECT name FROM employee WHERE salary > 2000 AND months < 10;
+
+-- https://www.hackerrank.com/challenges/salary-of-employees
