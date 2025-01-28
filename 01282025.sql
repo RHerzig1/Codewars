252bb6915620e4b48317b05dc4d987a5dcf6329b
Robin H <robin.hrzg@gmail.com>
2025-01-28 07:03:29 -0800

    Generate a list of each name seperated into columns by occupation.

---
+++ 01282025.sql
@@ -0,0 +1,31 @@
+-- Generate a list of each name seperated into columns by occupation.
+
+SELECT
+    MAX(Doctor) AS Doctor,
+    MAX(Professor) AS Professor,
+    MAX(Singer) AS Singer,
+    MAX(Actor) AS Actor
+FROM (
+    SELECT
+        CASE
+            WHEN Occupation = 'Doctor' THEN Name
+        END AS Doctor,
+        CASE
+            WHEN Occupation = 'Professor' THEN Name
+        END AS Professor,
+        CASE
+            WHEN Occupation = 'Singer' THEN Name
+        END AS Singer,
+        CASE
+            WHEN Occupation = 'Actor' THEN Name
+        END AS Actor,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS RowNumber
+    FROM
+        OCCUPATIONS
+) PIVOT_TABLE
+GROUP BY
+    RowNumber
+ORDER BY
+    RowNumber;
+
+-- https://www.hackerrank.com/challenges/occupations
