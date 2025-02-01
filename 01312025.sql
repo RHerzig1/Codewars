d4bb020d3a35f18703b53b0104161d92d093d3ea
Robin H <robin.hrzg@gmail.com>
2025-01-31 23:02:36 -0600

    Query the sum of latitudes.

---
+++ 01312025.sql
@@ -0,0 +1,10 @@
+-- Query the sum of latitudes.
+
+SELECT
+    ROUND(SUM(lat_n), 4)
+FROM
+    station
+WHERE
+    lat_n BETWEEN 38.7880 AND 137.2345;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-13
